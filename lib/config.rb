69a69da5c9f8caa3a61cdb8af871e4b39eea574a
Jenah <jenah.blitz@simplybusiness.com>
2021-03-23 11:58:30 -0400

    Shaaaaame

--- lib/config.rb
+++ lib/config.rb
@@ -4,10 +4,11 @@ require 'octokit'
 require 'json'
 # Github configurations
 class Config
-  attr_reader :client, :event_payload
+  attr_reader :client, :event_payload, :event_name
 
   def initialize
     @client = Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
     @event_payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+    @event_name = ENV['GITHUB_EVENT_NAME']
   end
 end
